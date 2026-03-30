69c9aa849b353611d2939907e43e8ef52d88ce05
Thejas <thejaskini68@gmail.com>
2026-03-30 20:58:27 +0530

    feat: add random password generator script

---
+++ password_generator.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    length = int(input("Enter password length: "))
+    print("\n🔐 Generated Password:")
+    print(generate_password(length))
